7d330e56bab457b4ebfc2f2b91dc92c2e44cc20f
mehmetfazil <mehmetfazil@outlook.com>
2025-02-10 12:56:44 +0000

    dirty fix to keep conn open

--- db/db.go
+++ db/db.go
@@ -3,12 +3,17 @@ package db
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	sqlitecloud "github.com/sqlitecloud/sqlitecloud-go"
 )
 
-var Db *sqlitecloud.SQCloud
+var (
+	Db              *sqlitecloud.SQCloud
+	keepAliveTicker *time.Ticker
+	quit            chan struct{}
+)
 
 func Init() {
 	if err := godotenv.Load(); err != nil {
@@ -25,12 +30,50 @@ func Init() {
 		log.Fatalf("Failed to connect to SQLite Cloud: %v", err)
 	}
 	log.Println("Successfully connected to SQLite Cloud")
+
+	// Start the keep-alive routine
+	startKeepAlive(5 * time.Minute) // Adjust the interval as needed
 }
 
-// CloseDB safely closes the database connection
+// startKeepAlive starts a goroutine that periodically checks the database connection.
+func startKeepAlive(interval time.Duration) {
+	keepAliveTicker = time.NewTicker(interval)
+	quit = make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case <-keepAliveTicker.C:
+				if _, err := Db.Select("SELECT 1;"); err != nil {
+					log.Printf("Keep-alive query failed: %v", err)
+					log.Println("Attempting to reconnect to SQLite Cloud...")
+					Db.Close()
+					connStr := os.Getenv("SQLITECLOUD_CONN_STR")
+					Db, err = sqlitecloud.Connect(connStr)
+					if err != nil {
+						log.Printf("Reconnection attempt failed: %v", err)
+					} else {
+						log.Println("Reconnected to SQLite Cloud")
+					}
+				} else {
+					log.Println("Keep-alive query succeeded")
+				}
+			case <-quit:
+				return
+			}
+		}
+	}()
+}
+
 func Close() {
 	if Db != nil {
 		Db.Close()
 		log.Println("Database connection closed")
+	}
+	if keepAliveTicker != nil {
+		keepAliveTicker.Stop()
+	}
+	if quit != nil {
+		close(quit)
 	}
 }
